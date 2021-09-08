baebede783b0cd783b5a4936095a26ccab9b3f41
Sof <sofkj@itu.dk>
2021-09-08 12:49:34 +0200

    added interface to isleapYear

--- leap-year-calculator.Tests/LeapYearCalculatorTests.cs
+++ leap-year-calculator.Tests/LeapYearCalculatorTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace leap_year_calculator.Tests
@@ -8,36 +9,59 @@ namespace leap_year_calculator.Tests
         [Fact]
         public void Year_4_is_leap()
         {
-            LeapYearCalculator calculator = new LeapYearCalculator();
-            Assert.True(calculator.IsLeapYear(4));
+            Assert.True(LeapYearCalculator.IsLeapYear(4));
         }
 
         [Fact]
         public void Year_divisible_by_4_is_leap()
         {
-            LeapYearCalculator calculator = new LeapYearCalculator();
-            Assert.True(calculator.IsLeapYear(1072));
+            Assert.True(LeapYearCalculator.IsLeapYear(1072));
         }
 
         [Fact]
-        public void Year_not_divisible_by_4_is_leap()
+        public void Year_not_divisible_by_4_is_not_leap()
         {
-            LeapYearCalculator calculator = new LeapYearCalculator();
-            Assert.False(calculator.IsLeapYear(2011));
+            Assert.False(LeapYearCalculator.IsLeapYear(2011));
         }
 
         [Fact]
         public void Year_divisible_by_100_not_leap() 
         {
-            LeapYearCalculator calculator = new LeapYearCalculator();
-            Assert.False(calculator.IsLeapYear(900));  
+            Assert.False(LeapYearCalculator.IsLeapYear(900));  
         }
 
         [Fact]
         public void Year_divisible_by_400_is_leap() 
         {
-            LeapYearCalculator calculator = new LeapYearCalculator();
-            Assert.True(calculator.IsLeapYear(800));  
+            Assert.True(LeapYearCalculator.IsLeapYear(800));  
+        }
+
+        [Fact]
+        public void Console_Prints_Nay() 
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("2021");
+            Console.SetIn(input);
+
+            LeapYearCalculator.Main(new string[0]);
+
+            Assert.Equal("nay", writer.GetStringBuilder().ToString().Substring(44).Trim());
+        }
+
+        [Fact]
+        public void Console_Prints_Yay() 
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("2024");
+            Console.SetIn(input);
+
+            LeapYearCalculator.Main(new string[0]);
+
+            Assert.Equal("yay", writer.GetStringBuilder().ToString().Substring(44).Trim());
         }
     }
 }
