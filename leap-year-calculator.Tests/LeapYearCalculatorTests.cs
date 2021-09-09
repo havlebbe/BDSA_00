239ff425fc05e7997b9013aff8330ded3e5c5bdd
havlebbe <74769975+havlebbe@users.noreply.github.com>
2021-09-09 17:04:20 +0200

    Implemented error handling (#5)

--- leap-year-calculator.Tests/LeapYearCalculatorTests.cs
+++ leap-year-calculator.Tests/LeapYearCalculatorTests.cs
@@ -6,16 +6,11 @@ namespace leap_year_calculator.Tests
 {
     public class LeapYearCalculatorTests
     {
-        [Fact]
-        public void Year_4_is_leap()
-        {
-            Assert.True(LeapYearCalculator.IsLeapYear(4));
-        }
 
         [Fact]
         public void Year_divisible_by_4_is_leap()
         {
-            Assert.True(LeapYearCalculator.IsLeapYear(1072));
+            Assert.True(LeapYearCalculator.IsLeapYear(1672));
         }
 
         [Fact]
@@ -27,13 +22,13 @@ namespace leap_year_calculator.Tests
         [Fact]
         public void Year_divisible_by_100_not_leap() 
         {
-            Assert.False(LeapYearCalculator.IsLeapYear(900));  
+            Assert.False(LeapYearCalculator.IsLeapYear(1700));  
         }
 
         [Fact]
         public void Year_divisible_by_400_is_leap() 
         {
-            Assert.True(LeapYearCalculator.IsLeapYear(800));  
+            Assert.True(LeapYearCalculator.IsLeapYear(1600));  
         }
 
         [Fact]
@@ -61,6 +56,25 @@ namespace leap_year_calculator.Tests
 
             LeapYearCalculator.Main(new string[0]);
             Assert.Contains("yay", writer.GetStringBuilder().ToString());
+        }
+
+        [Fact]
+        public void error_thrown_when_year_below_1582() 
+        {
+            Assert.Throws<ArgumentException>(() => LeapYearCalculator.IsLeapYear(4));
+        }
+
+        [Fact]
+        public void interface_informs_when_input_not_int() 
+        {
+             var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("hello");
+            Console.SetIn(input);
+
+            LeapYearCalculator.Main(new string[0]);
+            Assert.Contains("This is not a year", writer.GetStringBuilder().ToString());
         }
     }
 }
