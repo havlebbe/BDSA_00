530c0609530099ee1f87e9bafc3eafeac86dfa74
Sof <sofkj@itu.dk>
2021-09-09 17:01:13 +0200

    Implemented error handling

--- leap-year-calculator/LeapYearCalculator.cs
+++ leap-year-calculator/LeapYearCalculator.cs
@@ -7,12 +7,28 @@ namespace leap_year_calculator
         public static void Main(string[] args) 
         {
             Console.WriteLine("Please type a year to check if it is leap:");
-            int year = Convert.ToInt32(Console.ReadLine());
-            if (IsLeapYear(year)) Console.WriteLine("yay");
-            else Console.WriteLine("nay");
+            try 
+            {
+                int year = Convert.ToInt32(Console.ReadLine());
+                if (IsLeapYear(year)) Console.WriteLine("yay");
+                else Console.WriteLine("nay");
+            } 
+            catch(FormatException) 
+            {
+                Console.WriteLine("This is not a year");
+            } 
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Year must be after 1582");
+            }
         }
 
-        public static bool IsLeapYear(int year) {
+        public static bool IsLeapYear(int year)  
+        {
+            if (year < 1582) 
+            {
+                throw new ArgumentException("Year must be after 1582");
+            }
             bool isLeap = false;
             if (year % 4 == 0) isLeap = true;
             if (year % 100 == 0) isLeap = false;
